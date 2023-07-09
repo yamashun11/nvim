c780432d7c19c58536bfc8f403c879ac97f0f826
yamashun11 <yamashun113@gmail.com>
2023-07-10 01:34:18 +0900

    delete: autocmd packersync

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,9 +1,1 @@
--- TODO
---vim.cmd([[
-	--augroup packer_user_config
-		--autocmd!
-		--autocmd BufWritePost plugins.lua source <afile> | PackerSync
-	--augroup end
---]])
-
 vim.cmd("autocmd TermOpen * startinsert")
