eae5eb43bacee7fd9d6fa9dcf6b5d0f3a2c3443a
yamashun11 <yamashun113@gmail.com>
2024-02-12 22:34:51 +0900

    change: location of keymaps conf for nvim-cmp and gitsigns

--- lua/config/nvim-cmp_conf.lua
+++ lua/config/nvim-cmp_conf.lua
@@ -13,19 +13,23 @@ cmp.setup({
     -- completion = cmp.config.window.bordered(),
     -- documentation = cmp.config.window.bordered(),
   },
-  -- Keymap configurations have been moved to keymaps.lua
-  --[[
   mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),  -- Scroll the documentation window if visible
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),    -- Scroll the documentation window if visible
-    ["<C-p>"] = cmp.mapping.select_prev_item(),  -- Select the previous item. Set count with large number to select pageup
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Scroll the documentation window if visible
+    ["<C-f>"] = cmp.mapping.scroll_docs(4), -- Scroll the documentation window if visible
+    ["<C-p>"] = cmp.mapping.select_prev_item(), -- Select the previous item. Set count with large number to select pageup
     ["<C-n>"] = cmp.mapping.select_next_item(), -- Select the next item. Set count with large number to select pageup
-    ["<C-y>"] = cmp.mapping.complete(),      -- Invoke completion
-    ["<C-e>"] = cmp.mapping.abort(),      -- Closes the completion menu and restore the current line to the state before the current completion was started
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),   -- Accepts the currently selected completion item.
-                                                          -- If you didn't select any item and the option table contains `select = true`, nvim-cmp will automatically select the first item.
+    ["<C-y>"] = cmp.mapping.complete(), -- Invoke completion
+    ["<C-e>"] = cmp.mapping.abort(), -- Closes the completion menu and restore the current line to the state before the current completion was started
+    ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accepts the currently selected completion item.
+    -- If you didn't select any item and the option table contains `select = true`, nvim-cmp will automatically select the first item.
+    ["<C-k>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
-  ]]
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     --{ name = "vsnip" }, -- For vsnip users.
@@ -34,7 +38,7 @@ cmp.setup({
     --{ name = "ultisnips" }, -- For ultisnips users.
   }, {
     { name = "buffer" },
-  })
+  }),
 })
 
 --[[
@@ -51,18 +55,18 @@ cmp.setup.filetype("gitcommit", {
 cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = "buffer" }
-  }
+    { name = "buffer" },
+  },
 })
 
 -- Use cmdline % path source for ":"
 cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = "path" }
+    { name = "path" },
   }, {
-    { name = "cmdline" }
-  })
+    { name = "cmdline" },
+  }),
 })
 
 --[[
@@ -72,7 +76,7 @@ cmp.setup {
     format = lspkind.cmp_format({
       mode = 'symbol', -- show only symbol annotations
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                     -- can also be a function to dynamically calculate max width such as 
+                     -- can also be a function to dynamically calculate max width such as
                      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       show_labelDetails = true, -- show labelDetails in menu. Disabled by default
