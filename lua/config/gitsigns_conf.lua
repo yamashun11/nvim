8d7bcbe05715c431d9a81cdb4093762f80e203b1
yamashun <yamashun113@gmail.com>
2024-03-16 09:13:05 +0900

    fix: overlap of setup

--- lua/config/gitsigns_conf.lua
+++ lua/config/gitsigns_conf.lua
@@ -41,40 +41,38 @@ require("gitsigns").setup({
     enable = false,
   },
 
-  require("gitsigns").setup({
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map("n", "[c", function()
+      if vim.wo.diff then
+        return "[c"
       end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true })
 
-      -- Navigation
-      map("n", "[c", function()
-        if vim.wo.diff then
-          return "[c"
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true })
+    map("n", "]c", function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true })
 
-      map("n", "]c", function()
-        if vim.wo.diff then
-          return "]c"
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true })
-
-      -- Actions
-      map("n", "<leader>hd", gs.diffthis)
-      map("n", "<leader>td", gs.toggle_deleted)
-    end,
-  }),
+    -- Actions
+    map("n", "<leader>hd", gs.diffthis)
+    map("n", "<leader>td", gs.toggle_deleted)
+  end,
 })
