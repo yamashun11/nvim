8572e6501066fa4a1d216ca46225c7ac2e29b429
yamashun <yamashun113@gmail.com>
2024-09-06 03:05:28 +0900

    chore

--- lua/config/mason-lspconfig_conf.lua
+++ lua/config/mason-lspconfig_conf.lua
@@ -20,5 +20,5 @@ require("mason-lspconfig").setup_handlers({
 })
 
 -- virtual text
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+
