3dce5518a80a40b1316b9507262d4d22577dc47a
yamashun <yamashun113@gmail.com>
2024-09-06 03:06:01 +0900

    feature: add single border for hover

--- lua/config/mason-lspconfig_conf.lua
+++ lua/config/mason-lspconfig_conf.lua
@@ -22,3 +22,5 @@ require("mason-lspconfig").setup_handlers({
 -- virtual text
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
 
+-- border of hover
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single", })
