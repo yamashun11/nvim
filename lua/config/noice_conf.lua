49ad96ea0a773e4532603cf776fc83bb5aa3ccf7
yamashun <yamashun113@gmail.com>
2024-09-06 03:08:00 +0900

    fix: avoid to override settings of hover by noice

--- lua/config/noice_conf.lua
+++ lua/config/noice_conf.lua
@@ -1,12 +1,15 @@
 require("noice").setup({
-  --lsp = {
+  lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     --override = {
       --["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       --["vim.lsp.util.stylize_markdown"] = true,
       --["cmp.entry.get_documentation"] = true,
     --},
-  --},
+    hover = {
+      enabled = false,
+    },
+  },
   -- you can enable a preset for easier configuration
   presets = {
     bottom_search = false, -- use a classic bottom cmdline for search
