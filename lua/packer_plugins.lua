060a75eaff37f7482d73c0da3bfb11ce390e07b2
yamashun11 <yamashun113@gmail.com>
2025-03-19 05:14:28 +0000

    fix: deleted tag attribute

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -51,7 +51,6 @@ require("packer").startup({
     -- Fuzzy Finder
     use({
       "nvim-telescope/telescope.nvim",
-      tag = "0.1.5",
       requires = {
         "nvim-lua/plenary.nvim",
       },
