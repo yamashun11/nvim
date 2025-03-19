dd4a24f30613bd1db235e7289aaf808014914de7
yamashun11 <yamashun113@gmail.com>
2025-03-19 06:32:18 +0000

    feature: added autocmd to change the apperance of nvim-treesitter-context

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,2 +1,3 @@
 vim.cmd("autocmd TermOpen * startinsert")
 vim.cmd("autocmd FileType * setlocal formatoptions-=ro")
+vim.cmd("autocmd ColorScheme * highlight TreesitterContextBottom gui=underline guisp=Grey")
